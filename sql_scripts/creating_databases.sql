c000d26e54bdeed6ea78f776c73d49d039a5d72d
Sincobile <sibisisincobile6@gmail.com>
2026-04-09 19:46:39 +0200

    creating databases and tables

---
+++ sql_scripts/creating_databases.sql
@@ -0,0 +1,4 @@
+--creating databases
+create database computer_std
+create database computer_dhw
+
---
+++ sql_scripts/creating_tables.sql
@@ -0,0 +1,75 @@
+--creating tables
+
+--create table dim_channel on computer_std database
+
+SELECT 
+      [Channel]
+    into  [computer_std].[dbo].[dim_channel]
+      
+  FROM [computer_std].[dbo].[pc_dataset]
+
+  --create table dim_credit_score on computer_std database
+SELECT
+    
+      [Credit_Score]
+      
+      into [computer_std].[dbo].[dim_credit_score]
+  FROM [computer_std].[dbo].[pc_dataset]
+
+  --creating table dim_customers on computer_std database
+
+SELECT  
+      [Customer_Name]
+      ,[Customer_Surname]
+      ,[Customer_Contact_Number]
+      ,[Customer_Email_Address]
+       into [computer_std].[dbo].[dim_customer]
+  FROM [computer_std].[dbo].[pc_dataset]
+
+--create table dim_location on computer_std database
+SELECT  [Continent]
+      ,[Country_or_State]
+      ,[Province_or_City]
+      into [computer_std].[dbo].[dim_location]
+      FROM [computer_std].[dbo].[pc_dataset]
+
+--create table dim_pc on computer_std
+
+SELECT
+      [PC_Make]
+      ,[PC_Model]
+      ,[Storage_Type]
+      ,[RAM]
+      ,[Storage_Capacity]
+      into [computer_std].[dbo].[dim_pc]
+  FROM [computer_std].[dbo].[pc_dataset]
+
+
+--create table dim_priority on computer_std database
+
+SELECT 
+      
+      [Priority]
+     into [computer_std].[dbo].[dim_priority] 
+  FROM [computer_std].[dbo].[pc_dataset]
+
+  --create table dim_shop on computer_std database
+SELECT  
+      [Shop_Name]
+      ,[Shop_Age]
+      into [computer_std].[dbo].[dim_shop]
+  FROM [computer_std].[dbo].[pc_dataset]
+
+--creating table orders_fact on computer_std database
+SELECT [Cost_Price]
+      ,[Sale_Price]
+      ,[Discount_Amount]
+      ,[Purchase_Date]
+      ,[Ship_Date]
+      ,[Finance_Amount]
+      ,[Cost_of_Repairs]
+      ,[Total_Sales_per_Employee]
+      ,[PC_Market_Price]
+
+     into [computer_std].[dbo].[orders_fact]
+  FROM [computer_std].[dbo].[pc_dataset]
