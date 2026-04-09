072753101654c733c872ee61db1e7ead66fbdc49
Sincobile <sibisisincobile6@gmail.com>
2026-04-09 20:09:23 +0200

    modifying table dim_credit_score and table orders_fact

--- sql_scripts/creating_tables.sql
+++ sql_scripts/creating_tables.sql
@@ -2,23 +2,20 @@
 
 --create table dim_channel on computer_std database
 
-SELECT 
+SELECT distinct
       [Channel]
     into  [computer_std].[dbo].[dim_channel]
       
   FROM [computer_std].[dbo].[pc_dataset]
 
   --create table dim_credit_score on computer_std database
-SELECT
-    
-      [Credit_Score]
-      
-      into [computer_std].[dbo].[dim_credit_score]
-  FROM [computer_std].[dbo].[pc_dataset]
+  --drop table dim_credit_score
+     drop table [computer_std].[dbo].[dim_credit_score]
+ 
 
   --creating table dim_customers on computer_std database
 
-SELECT  
+SELECT  distinct
       [Customer_Name]
       ,[Customer_Surname]
       ,[Customer_Contact_Number]
@@ -27,7 +24,7 @@ SELECT
   FROM [computer_std].[dbo].[pc_dataset]
 
 --create table dim_location on computer_std database
-SELECT  [Continent]
+SELECT distinct  [Continent]
       ,[Country_or_State]
       ,[Province_or_City]
       into [computer_std].[dbo].[dim_location]
@@ -35,7 +32,7 @@ SELECT  [Continent]
 
 --create table dim_pc on computer_std
 
-SELECT
+SELECT distinct
       [PC_Make]
       ,[PC_Model]
       ,[Storage_Type]
@@ -47,21 +44,22 @@ SELECT
 
 --create table dim_priority on computer_std database
 
-SELECT 
+SELECT distinct
       
       [Priority]
      into [computer_std].[dbo].[dim_priority] 
   FROM [computer_std].[dbo].[pc_dataset]
 
   --create table dim_shop on computer_std database
-SELECT  
+SELECT  distinct
       [Shop_Name]
       ,[Shop_Age]
       into [computer_std].[dbo].[dim_shop]
   FROM [computer_std].[dbo].[pc_dataset]
 
 --creating table orders_fact on computer_std database
-SELECT [Cost_Price]
+SELECT 
+[Cost_Price]
       ,[Sale_Price]
       ,[Discount_Amount]
       ,[Purchase_Date]
@@ -70,6 +68,23 @@ SELECT [Cost_Price]
       ,[Cost_of_Repairs]
       ,[Total_Sales_per_Employee]
       ,[PC_Market_Price]
+     into [computer_std].[dbo].[orders_fact]
+  FROM [computer_std].[dbo].[pc_dataset]
 
+--drop table orders_fact
+drop table [computer_std].[dbo].[orders_fact]
+  
+
+  SELECT distinct
+[Cost_Price]
+      ,[Sale_Price]
+      ,[Discount_Amount]
+      ,[Purchase_Date]
+      ,[Ship_Date]
+      ,[Finance_Amount]
+      ,[Cost_of_Repairs]
+      ,[Total_Sales_per_Employee]
+      ,[PC_Market_Price]
+      , [Credit_Score]
      into [computer_std].[dbo].[orders_fact]
   FROM [computer_std].[dbo].[pc_dataset]
